b0d00b800e7cae91fb0a3a02b9b06cbd1ed595b0
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-03-28 19:24:51 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise ((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  })
+  
+}
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  let time = Number(formData.delay);
+  for (let i = 0; i < formData.amount; i += 1) {
+    setTimeout(() => {
+      createPromise(i + 1, time + i * Number(formData.step))
+      .then(message => Notiflix.Notify.success(message))
+      .catch(e => Notiflix.Notify.failure(e));
+    }, time + i * Number(formData.step));
   }
 }
+
+let formData = {};
+const ref = {
+  form: document.querySelector('.form')
+}
+
+ref.form.addEventListener('submit', onFormSubmit);
+ref.form.addEventListener('input', onFormInput);
+
